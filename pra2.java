251de31efe14021b1349e2081b2046ef88830139
Ryo <146405208+hesogoma2nd@users.noreply.github.com>
2023-10-08 05:51:38 +0900

    Update pra2.java

--- pra2.java
+++ pra2.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
             System.out.println("Hello, World!");
